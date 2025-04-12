0f9c764b8cd05084547cdf1c3a9f29f9249b5bc6
Cristian Ordoñez <cordonezjolon@gmail.com>
2025-04-12 08:44:56 -0600

    feat: change name to new version.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/ping', (req, res) => {
-  res.json({ message: 'pong deploy againg' });
+  res.json({ message: 'pong deploy againg restart' });
 });
 
 app.listen(port, () => {
