5c115aeac201e5cf2093c451c18e77c429bd7041
Cristian Ordoñez <cordonezjolon@gmail.com>
2025-04-12 08:37:07 -0600

    feat: change message to response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/ping', (req, res) => {
-  res.json({ message: 'pong deploy' });
+  res.json({ message: 'pong deploy againg' });
 });
 
 app.listen(port, () => {
