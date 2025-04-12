4e0ec20929b208573b7b2c50e07aebb5d9ffbb53
Cristian Ordoñez <cordonezjolon@gmail.com>
2025-04-12 10:12:40 -0600

    feat: add metrics for pod

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const port = process.env.PORT || 3000;
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong deploy againg restart' });
 });
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+  });
 
 app.listen(port, () => {
   console.log(`API corriendo en http://localhost:${port}`);
